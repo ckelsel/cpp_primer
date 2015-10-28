36363efd9eb888e842a399b585cd07d38e622319
ckelsel <ckelsel@hotmail.com>
2015-10-28 08:56:17 +0800

    const double PI=3.14; in e_p102.h

---
+++ src/chapter_two/e_p102.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "e_p102.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    double tmp = PI;
+    return 0;
+}
---
+++ src/chapter_two/e_p102.h
@@ -0,0 +1,7 @@
+#ifndef _E_P02_H__
+#define _E_P02_H__
+
+const double PI = 3.1415;
+
+#endif // _E_P02_H__
+
