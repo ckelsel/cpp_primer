7f07a524e8cef90203d14b6b3ffe589c058d3c9a
ckelsel <ckelsel@hotmail.com>
2015-11-19 09:48:01 +0800

    e12_11 p556

---
+++ src/chapter_twelve/p556.cpp
@@ -0,0 +1,89 @@
+/**-------------------------------------------------------------------------
+ * Copyright (C) 2015 KunMing Xie <ckelsel@hotmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Persion;
+class Doctor {
+    //  declaration , define
+    Persion *persion1_;
+
+    /*
+     * Persion persion;
+     * Persion *persion;
+     * Persion &persion;
+     */
+
+    //  declaration , define
+    void put(Persion *persion){}
+
+    //  declaration , define
+    void put2(Persion &persion){}
+
+    //  declaration 
+    void put3(Persion persion);
+
+
+    //  declaration , define
+    Persion *get(){ return (Persion *)NULL; }
+
+    //  declaration 
+    Persion &get2();
+
+    //  declaration 
+    Persion get3();
+};
+
+class Persion {
+private:
+    string name_;
+    string address_;
+
+public:
+    Persion(){}
+    ~Persion(){}
+
+    Persion(string name, string address);
+
+    string getName() const;
+    string getAddress() const;
+};
+
+Persion::Persion(string name, string address)
+{
+    name_ = name;
+    address_ = address;
+}
+
+string Persion::getName() const
+{
+    return name_;
+}
+
+string Persion::getAddress() const
+{
+    return address_;
+}
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
+
